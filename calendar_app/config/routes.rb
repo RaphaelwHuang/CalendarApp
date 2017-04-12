45bc002c254a41a5f1f783198a60fdcd50d1de32
sunnybpatel <patel.2222@osu.edu>
2017-04-12 19:21:19 -0400

    Fixed routes for devise. Added static_pages controller for static pages like home

--- calendar_app/config/routes.rb
+++ calendar_app/config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root 'static_pages#home'
+  devise_for :users,
+    path: '',
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout'
+    }
   resources :schedules
   resources :administrates
   resources :comprises
   resources :groups
   resources :events
   resources :calendars
-  root 'users#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
