1a767db71e3854e903da7e32f6d1a0006e2f6878
RaphaelwHuang <raphaelhuang22@gmail.com>
2017-04-14 19:46:03 -0400

    Changed the URL for help page to the named routes and fixed the bugs: 1. the edit can not work for add_event; 2. user can enter anything for start_day/end_day for add_event.

--- calendar_app/app/models/event.rb
+++ calendar_app/app/models/event.rb
@@ -1,2 +1,10 @@
 class Event < ActiveRecord::Base
+  validates :name, length: { minimum: 0 },
+            presence: true
+
+  validates :start_day, inclusion: { in: %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday),
+                                     message: "%{value} is not a valid week day" }
+
+  validates :end_day,  inclusion: { in: %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday),
+                                    message: "%{value} is not a valid week day"  }
 end
