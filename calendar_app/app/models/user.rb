95c77c211190f5f2a549598bf1074b843d9b101f
Kenton Steiner <steiner.257@osu.edu>
2017-04-14 16:56:36 -0400

    Fixed the user.rb and removed lockable and confirmable to get login and account creation to work again

--- calendar_app/app/models/user.rb
+++ calendar_app/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :omniauthable
-  devise :confirmable, :lockable, :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :timeoutable
 end
