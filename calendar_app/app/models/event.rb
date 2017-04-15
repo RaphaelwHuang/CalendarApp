2f0d08b1f2d13cd2db132694e7d5b018e6a23b63
sunnybpatel <patel.2222@osu.edu>
2017-04-14 21:13:39 -0400

    Fixed regexp for days of the week. Fixed bug so event form works for edits now.

--- calendar_app/app/models/event.rb
+++ calendar_app/app/models/event.rb
@@ -2,9 +2,9 @@ class Event < ActiveRecord::Base
   validates :name, length: { minimum: 0 },
             presence: true
 
-  validates :start_day, inclusion: { in: %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday),
+  validates :start_day, format: { with: /(mon|tues|wednes|thurs|fri|satur|sun)(day)/i,
                                      message: "%{value} is not a valid week day" }
 
-  validates :end_day,  inclusion: { in: %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday),
+  validates :end_day,  inclusion: { in: /(mon|tues|wednes|thurs|fri|satur|sun)(day)/i,
                                     message: "%{value} is not a valid week day"  }
 end
