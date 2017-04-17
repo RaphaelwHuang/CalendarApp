72adbea1e88818e0d36c787545ee9137191f7ca5
RaphaelwHuang <huang.2339@osu.edu>
2017-04-17 00:33:00 -0400

    Added stylesheets for events page and personal page.

--- calendar_app/app/helpers/personal_helper.rb
+++ calendar_app/app/helpers/personal_helper.rb
@@ -1,2 +1,23 @@
 module PersonalHelper
+  def find_month
+    Time.now.strftime("%B")
+  end
+
+  def find_day
+    week = Time.now.strftime("%U").to_i
+    wkBegin = Date.commercial(2017, week, 1).day
+    wkEnd = Date.commercial(2017, week, 7).day
+
+    find_month + " " + wkBegin.to_s + " to " + find_month + " " + wkEnd.to_s
+  end
+
+  def find_week
+    year = Time.now.year.to_s
+
+    year + " WEEK " + Time.now.strftime("%U").to_s
+  end
+
 end
+
+
+
