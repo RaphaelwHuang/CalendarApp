586d71ea86ad1574edf8fde686a5fa2e3cb0b6b2
sunnybpatel <patel.2222@osu.edu>
2017-04-19 12:18:38 -0400

    properly merged user.rb

--- calendar_app/app/models/user.rb
+++ calendar_app/app/models/user.rb
@@ -3,12 +3,8 @@ class User < ActiveRecord::Base
   # :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :timeoutable
-<<<<<<< HEAD
   validates :fname, presence: true, format: {with: /\A[a-zA-Z]+\z/, message: "Firstname can only be letter"}
   has_many :events
-=======
-  validates :fname, presence: true, format: {with: /\A[a-zA-Z]+\z/, message: "Firstname can only be letter"}
->>>>>>> d99fca787a0ef4c4fe73e427baee835246b88349
   has_many :comprises
   has_many :administrates
   has_many :groups, through: :comprises
