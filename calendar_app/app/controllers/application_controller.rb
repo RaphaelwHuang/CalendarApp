7b444705bbb3b1febaeb18552d66507de287ab66
jennalarcon <alarcon.11@osu.edu>
2017-04-20 13:42:18 -0400

    Sunny and Jenn fixed error causing profile page not to laod, by removing authentication from application controller.

--- calendar_app/app/controllers/application_controller.rb
+++ calendar_app/app/controllers/application_controller.rb
@@ -3,12 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  protected
-  def authenticate_user!
-    if user_signed_in?
-      super
-    else
-      redirect_to root_path, :notice => 'Please log in or sign up!'
-    end
-  end
 end
