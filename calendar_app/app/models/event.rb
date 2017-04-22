5fec77ecaf060e9ca409871d9eb9ca2013d1221a
jennalarcon <alarcon.11@osu.edu>
2017-04-22 10:58:38 -0400

    Removed end_day from being an option

--- calendar_app/app/models/event.rb
+++ calendar_app/app/models/event.rb
@@ -12,13 +12,11 @@ class Event < ActiveRecord::Base
   validates :start_day, format: { with: /(mon|tues|wednes|thurs|fri|satur|sun)(day)/i,
                                      message: "%{value} is not a valid week day" }
 
-  validates :end_day, format: { with: /(mon|tues|wednes|thurs|fri|satur|sun)(day)/is,
-                                    message: "%{value} is not a valid week day"  }
 
   validate :check_time
 
   def check_time
-    errors.add(:end_time, "should be the greater than start time.") if end_time.to_i <= start_time.to_i  
+    errors.add(:end_time, "should be the greater than start time.") if end_time.to_i <= start_time.to_i
   end
 
 end
