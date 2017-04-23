82c6e5fc747b794099865d75a29d94f8e4ac9c9e
jennalarcon <alarcon.11@osu.edu>
2017-04-22 23:42:18 -0400

    Simple cal gem working

--- calendar_app/app/models/event.rb
+++ calendar_app/app/models/event.rb
@@ -1,14 +1,13 @@
-# Author: RH
+
+# Author:
 # Changes: Added the validation for the end_time
 
 class Event < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
-  has_many :schedules
 
-  validates :name, length: { minimum: 0 },
+  validates :name, length: { minimum: 1, maximum: 20 },
             presence: true
-
 
 
 
